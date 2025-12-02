4788c7030dbe6d8bef9aec2217776667b66cdeaf
willianmarc <willian.marcondes@hotmail.com>
2025-12-02 18:29:08 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fileinput import close
+
+import pygame
+print("Setup Start")
+pygame.init()
+window =pygame.display.set_mode(size=(600 , 480)) #para abrir uma janela e conf o atamanho dela
+print("Setup End")
+
+print("Loop Start")
+while True: #Chechar eventos
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: #Fechar janela
+          pygame.quit()
+          quit() #encerrar programa
