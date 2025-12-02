254caae4fe80becbd47e720d8220a813a32da98a
Willian <125850216+willianmarc@users.noreply.github.com>
2025-12-02 18:38:06 -0300

    Add quitting message in event loop

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ print("Setup End")
 print("Loop Start")
 while True: #Chechar eventos
     for event in pygame.event.get():
+        print('quitting...')
       if event.type == pygame.QUIT: #Fechar janela
           pygame.quit()
           quit() #encerrar programa
