78120b86a3a124f7c7f1d16b12950117e722d0b8
willianmarc <willian.marcondes@hotmail.com>
2025-12-04 17:41:26 -0300

    Fixing imports in level.py file

--- code/Menu.py
+++ code/Menu.py
@@ -12,7 +12,7 @@ from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')  # carregando a imagem do menu
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()  # carregando a imagem do menu
         self.rect = self.surf.get_rect(left=0, top=0)  # criar o retangulo
 
     def run(self,):
--- code/level.py
+++ code/level.py
@@ -1,27 +1,70 @@
-from importlib.util import source_hash
-from operator import truediv
-from sys import deactivate_stack_trampoline
+import sys
+
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.const import WIN_HEIGHT
 from xml.dom.minidom import Entity
 
 import pygame.display
 
 from code.EntityFactory import EntityFactory
+from code.const import COLOR_WHITE
 
 
 class Level:
-    def __init__(self, window,name,game_mode):
-        self.window =window
+    def __init__(self, window, name, game_mode):
+        self.window = window
         self.name = name
-        self.game_modev=game_mode
-        self.entity_list : list[Entity]=[]
+        self.game_mode = game_mode
+        self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-
+        self.timeout = 20000  # 20 segundos
 
     def run(self, ):
+        pygame.mixer_music.load(f'./asset/{self.name}.mp3')  # por musica na primeira fase
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
-                self.window.blit(source=ent.surf,dest=ent.rect)
+                self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # Texto do topo
+            self.level_text(
+                text_size=14,
+                text=f'{self.name} - Timeout: {self.timeout / 100:.1f}s',
+                text_color=COLOR_WHITE,
+                text_center_pos=(120, 10)
+            )
+
+            # FPS
+            self.level_text(
+                text_size=14,
+                text=f'fps: {clock.get_fps():.0f}',
+                text_color=COLOR_WHITE,
+                text_center_pos=(60, WIN_HEIGHT - 35)
+            )
+
+            # Quantidade de entidades
+            self.level_text(
+                text_size=14,
+                text=f'entidades: {len(self.entity_list)}',
+                text_color=COLOR_WHITE,
+                text_center_pos=(90, WIN_HEIGHT - 20)
+            )
+
             pygame.display.flip()
-            pass
         pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
